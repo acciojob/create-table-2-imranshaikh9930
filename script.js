810200f6dc1668c5299e870d1bf49045dd62fd60
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-31 14:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+const table = document.querySelector("#myTable");
 function createTable() {
-    //Write your code here
-  
+   let rows = prompt("input number of rows");
+	let cols = prompt("input number of columns")
+  // // console.log(value);
+  for(let i = 0; i<rows;i++){
+    const row = document.createElement("tr");
+    
+  for(let j = 0 ; j< cols;j++){
+    const col = document.createElement("td");
+
+    col.innerText = `Row${i}Column${j}`
+
+
+    row.appendChild(col);
+  }
+    table.appendChild(row);
+
+  }
+
+  console.log(table);
+
+  //  Columns
+
 }
